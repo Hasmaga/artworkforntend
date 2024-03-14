44e428fa0782440d071c1f8e97f601c9777bf91e
fatnotfat <nguyenphat2711@gmail.com>
2024-03-14 21:05:53 +0700

    init

---
+++ app/creator/page.tsx
@@ -0,0 +1,14 @@
+
+import BodyHomePage from "../ui/BodyHomePage/BodyHomePage";
+import Navbar from "../ui/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100">
+      <Navbar />
+      <div className="">
+        <BodyHomePage />
+      </div>
+    </div>
+  );
+}
--- app/ui/Navbar/Navbar.tsx
+++ app/ui/Navbar/Navbar.tsx
@@ -5,19 +5,32 @@ import Logo from '@/public/next.svg'
 import Link from 'next/link'
 import Home from '@/public/home.svg'
 import Create from '@/public/create-note-svgrepo-com.svg'
+import { GetRoleAsync } from '@/app/component/api/GetRoleAsync';
 
 export default function Navbar() {
   const [token, setToken] = useState<string | null>(null);
-
+  const [role, setRole] = useState<string | undefined>(undefined);
   useEffect(() => {
-    setToken(localStorage.getItem('token'));
+    const fetchRole = async () => {
+      const token = localStorage.getItem('token');
+      setToken(token);
+      if (token) {
+        const role = await GetRoleAsync(token);
+        console.log(role);
+        if (role.status === 'SUCCESS') {
+          setRole(role.data);
+        }
+      }
+    };
+    fetchRole();
+
   }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     if (typeof window !== 'undefined') {
       window.location.reload();
-    }  
+    }
   };
 
   return (
@@ -27,14 +40,22 @@ export default function Navbar() {
           <Image src={Logo} alt="logo" width={80} height={75} />
         </Link>
       </div>
-
       <div className="flex flex-row space-x-5 pr-20 justify-items-center ">
         <Link href="/" className="p-1">
-            <Image src={Home} alt="home" width={50} height={50} />
-        </Link> 
+          <Image src={Home} alt="home" width={50} height={50} />
+        </Link>
         <Link href="/create" className="p-1">
-            <Image src={Create} alt="create" width={50} height={50} />
-        </Link>           
+          <Image src={Create} alt="create" width={50} height={50} />
+        </Link>
+      </div>
+
+
+      <div className='flex flex-row space-x-5'>
+        {role === "CREATOR" ? (
+           <Link href="/login" className="p-3 shadow-xl bg-green-500 text-white rounded-md">Tạo tranh bán</Link>
+        ) : (
+          <div></div>
+        )}
       </div>
 
       <div className='flex flex-row space-x-5'>
